9c0b39a2a22b5fb621474f8b127ec5b449f85340
skandaPrasad <skandaprasad7@gmail.com>
2022-01-21 11:45:21 +0530

    adding dingdong

--- present.c
+++ present.c
@@ -7,7 +7,7 @@ int present(char st[],char n)
     {
         if(*ptr==n)
         {
-            printf("the character %c is present in the string",n);
+            printf("the character %c is present in the string dingdong",n);
            x++;
         }
         ptr++;
