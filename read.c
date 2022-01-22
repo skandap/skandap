5cd28d2c3ddef6c92b64048f4567bb067c6fe0a1
skandaPrasad <skandaprasad7@gmail.com>
2022-01-22 11:17:50 +0530

    commit

--- read.c
+++ read.c
@@ -14,8 +14,8 @@ int main()
         fscanf(ptr,"%d",&num1);
         fscanf(ptr,"%d",&num2);
         fclose(ptr);
-        printf("the value of num1 is %d\n",&num1);
-        printf("the value of num1 is %d\n",&num2);
+        printf("the value of num1 is %d\n",num1);
+        printf("the value of num1 is %d\n",num2);
         
     }
     return 0;
