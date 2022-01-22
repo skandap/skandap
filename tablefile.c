7417a3b1126844ee24780c98d40ad3347f771cac
skandaPrasad <skandaprasad7@gmail.com>
2022-01-22 12:11:35 +0530

    commit

--- tablefile.c
+++ tablefile.c
@@ -6,7 +6,7 @@ int num;
 ptr=fopen("apple.txt","w");
 printf("enter the number wich to be print the multiplication table");
 scanf("%d",&num);
-for(int i=1;i<10;i++)
+for(int i=1;i<=10;i++)
 {
 fprintf(ptr,"%d*%d = %d\n",num,i,num*(i));
 }
