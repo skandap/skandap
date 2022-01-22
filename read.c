23f4071fad87b6c9f0ca181ad0126a4274834cd2
skandaPrasad <skandaprasad7@gmail.com>
2022-01-22 11:14:49 +0530

    commit

---
+++ read.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    FILE *ptr;
+    int num1;
+    int num2;
+    ptr=fopen("SKANDA.txt","r");
+    if(ptr==NULL)
+    {
+        printf("this file does not exist");
+    }
+    else
+    {
+        fscanf(ptr,"%d",&num1);
+        fscanf(ptr,"%d",&num2);
+        fclose(ptr);
+        printf("the value of num1 is %d\n",&num1);
+        printf("the value of num1 is %d\n",&num2);
+        
+    }
+    return 0;
+    
+}
